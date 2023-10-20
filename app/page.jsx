66d30490460ad6694a93728075ced741e38d1a29
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-10-20 21:15:38 +0530

    clerk-auth

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-2xl font-bold mb-5'>Welcome Into Better-One's</h1>
+      <p className='mb-5'>
+        Updating Soon!!!
+      </p>
+    </>
+  );
+}
